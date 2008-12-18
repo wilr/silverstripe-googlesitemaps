71e1fddc6d0d26717dbc0e5829054e73974393a7
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2008-12-18 21:07:07 +0000

    BUGFIX Hide pages without view permissions from sitemap.xml. Thanks simon_w! (see #3234)

--- code/GoogleSitemap.php
+++ code/GoogleSitemap.php
@@ -44,9 +44,9 @@ class GoogleSitemap extends Controller {
 		foreach($this->Pages as $page) {
 			// Only include pages from this host and pages which are not an instance of ErrorPage 
 			if(parse_url($page->AbsoluteLink(), PHP_URL_HOST) == $_SERVER['HTTP_HOST'] && !($page instanceof ErrorPage)) {
-			
+
 				// If the page has been set to 0 priority, we set a flag so it won't be included
-				if(!isset($page->Priority) || $page->Priority > 0) {
+				if($page->canView() && (!isset($page->Priority) || $page->Priority > 0)) { 
 					// The one field that isn't easy to deal with in the template is
 					// Change frequency, so we set that here.
 					$properties = $page->toMap();
