2f271059d9fdc2b17edd8c875b08b8a187d82fcc
wrossiter <wrossiter@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2009-08-12 03:51:43 +0000

    MINOR: removed will scott as maintainer. BUGFIX: separated decorator file and main file. MINOR updated changelog

--- _config.php
+++ _config.php
@@ -1,5 +1,10 @@
 <?php
+
+// adds a rule to make www.site.com/sitemap.xml work
 Director::addRules(10, array(
 	'sitemap.xml' => 'GoogleSitemap',
 ));
+
+// add the extension 
+Object::add_extension('SiteTree', 'GoogleSitemapDecorator');
 ?>
