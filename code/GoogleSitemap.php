a8d1e91ddfb630e143d618fcfccaf0c2455ef622
mandrew <mandrew@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-02-02 22:28:11 +0000

    REVERT reverted back to origional SS_Datetime function

--- code/GoogleSitemap.php
+++ code/GoogleSitemap.php
@@ -62,9 +62,9 @@ class GoogleSitemap extends Controller {
 						// The one field that isn't easy to deal with in the template is
 						// Change frequency, so we set that here.
 						$properties = $page->toMap();
-						$created = new SSDatetime();
+						$created = new SS_Datetime();
 						$created->value = $properties['Created'];
-						$now = new SSDatetime();
+						$now = new SS_Datetime();
 						$now->value = date('Y-m-d H:i:s');
 						$versions = $properties['Version'];
 						$timediff = $now->format('U') - $created->format('U');
