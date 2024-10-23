675df31ec8bdd9a010115c47a7d3c04fe50884ea
Will Rossiter <will@fullscreen.io>
2024-10-24 06:34:28 +1300

    chore: code formatting (fixes #199)

--- _config.php
+++ _config.php
@@ -3,8 +3,10 @@
 use SilverStripe\Core\ClassInfo;
 use Wilr\GoogleSitemaps\GoogleSitemap;
 
-if (0 === strpos(ltrim($_SERVER['REQUEST_URI'], '/'), 'sitemap')) {
-    foreach (ClassInfo::implementorsOf(Sitemapable::class) as $className) {
-        GoogleSitemap::register_dataobject($className);
+if (array_key_exists('REQUEST_URI', $_SERVER)) {
+    if (0 === strpos(ltrim($_SERVER['REQUEST_URI'], '/'), 'sitemap')) {
+        foreach (ClassInfo::implementorsOf(Sitemapable::class) as $className) {
+            GoogleSitemap::register_dataobject($className);
+        }
     }
 }
