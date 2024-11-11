82406ff9c1d9816eeb8ceacf92156eac63aec142
Joe Chenevey <jchenevey@users.noreply.github.com>
2024-11-11 11:58:41 -0500

    Update _config.php
    Add use reference to top of file to ensure correct Interface is checked for class implementors.

--- _config.php
+++ _config.php
@@ -2,6 +2,7 @@
 
 use SilverStripe\Core\ClassInfo;
 use Wilr\GoogleSitemaps\GoogleSitemap;
+use Wilr\GoogleSitemaps\Sitemapable;
 
 if (array_key_exists('REQUEST_URI', $_SERVER)) {
     if (0 === strpos(ltrim($_SERVER['REQUEST_URI'], '/'), 'sitemap')) {
